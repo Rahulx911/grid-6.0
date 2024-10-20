a604f83fcbf7217e900ef228ef918fa4a706813d
Tanya <tanya.nijhawan2021@vitstudent.ac.in>
2024-10-20 14:38:04 +0530

    change

--- backend/routes/detect.py
+++ backend/routes/detect.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, jsonify
 from backend.models.object_detection import detect_objects
 import os
 from werkzeug.utils import secure_filename
+from io import BytesIO
+from PIL import Image
 
 # Create a blueprint for the route
 detect_objects_blueprint = Blueprint('detect_objects', __name__)
@@ -11,20 +13,31 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 @detect_objects_blueprint.route('/detect_objects', methods=['POST'])
+# def detect():
+#     if 'file' not in request.files:
+#         return jsonify({'error': 'No file uploaded'}), 400
+
+#     file = request.files['file']
+#     image = Image.open(BytesIO(file.read()))
+
+#     # Run object detection model
+#     boxes, total_objects = detect_objects(image)
+
+#     return jsonify({
+#         'total_objects': total_objects,
+#         'boxes': boxes.tolist()
+#     })
 def detect():
     if 'file' not in request.files:
         return jsonify({'error': 'No file uploaded'}), 400
 
-    # Secure the uploaded file name
     file = request.files['file']
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(UPLOAD_FOLDER, filename)
-    file.save(file_path)
+    image = Image.open(BytesIO(file.read()))
 
     # Run object detection model
-    boxes, total_objects = detect_objects(file_path)
+    boxes, total_objects = detect_objects(image)
 
     return jsonify({
         'total_objects': total_objects,
-        'boxes': boxes.tolist()  # Optional, return bounding boxes as well
+        'boxes': boxes.tolist()
     })
